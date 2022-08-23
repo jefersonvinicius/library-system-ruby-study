182f4dbf2162689edd0fe4b513b63e68b5bb9f87
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-08-22 23:09:42 -0300

    fix sorting method

--- app/shared/sorting.rb
+++ app/shared/sorting.rb
@@ -1,19 +1,36 @@
 class Sorting
     def self.sort(models:, changing:, to:)
-        return [] if to > models.length || to < 0 || changing.position == to
+        return [] if to > models.length || to < 0
 
-        target_model = models[to]
         changing_model = models.find { |model| model.id == changing.id }
+        return [] if changing_model.position == to 
+                     
+
+        self.fill_nil_positions(models)
+        # binding.pry
+        sorted = models.sort_by(&:position)
+        # binding.pry
+
+        target_model = sorted[to]
         
         is_sorting_to_ahead = to > changing_model.position
         start = is_sorting_to_ahead ? changing_model.position + 1 : to
         final = is_sorting_to_ahead ? to : changing_model.position - 1
         factor = is_sorting_to_ahead ? -1 : 1
 
-        to_update = models[start..final]
+        to_update = sorted[start..final]
         to_update.each { |model| model.position += factor }
         changing_model.position = to
 
         return [changing_model, *to_update]
+    end
+
+    def self.fill_nil_positions(items) 
+        items_with_nil_positions = items.select { |item| item.position.nil? }
+        index = items.length - items_with_nil_positions.length
+        items_with_nil_positions.each do |item|
+            item.position = index
+            index += 1
+        end
     end
 end
--- spec/shared/sorting_spec.rb
+++ spec/shared/sorting_spec.rb
@@ -4,7 +4,7 @@ class FakeModel
     attr_reader :id
     attr_accessor :position
 
-    def initialize(id:, position:)
+    def initialize(id:, position: nil)
         @id = id
         @position = position
     end
@@ -149,6 +149,102 @@ RSpec.describe 'Sorting' do
                 FakeModel.new(id: 10, position: 9),
             ])
             expect(changed.map(&:id)).to eq([])
+        end
+    end
+
+    context 'with out of order models' do
+        let(:model) { FakeModel.new id: 5 }
+        let(:models) {[
+            FakeModel.new(id: 1, position: 0), 
+            FakeModel.new(id: 2, position: 1), 
+            FakeModel.new(id: 4, position: 3),
+            FakeModel.new(id: 5, position: 4),
+            FakeModel.new(id: 10, position: 9),
+            FakeModel.new(id: 7, position: 6),
+            FakeModel.new(id: 6, position: 5),
+            FakeModel.new(id: 8, position: 7),
+            FakeModel.new(id: 3, position: 2),
+            FakeModel.new(id: 9, position: 8)
+        ]}
+
+        it 'should sorting to position ahead' do
+            changed = Sorting.sort models: models, changing: model, to: 8
+
+            expect(models).to eq([
+                FakeModel.new(id: 1, position: 0), 
+                FakeModel.new(id: 2, position: 1), 
+                FakeModel.new(id: 4, position: 3),
+                FakeModel.new(id: 5, position: 8),
+                FakeModel.new(id: 10, position: 9),
+                FakeModel.new(id: 7, position: 5),
+                FakeModel.new(id: 6, position: 4),
+                FakeModel.new(id: 8, position: 6),
+                FakeModel.new(id: 3, position: 2),
+                FakeModel.new(id: 9, position: 7),
+            ])
+            expect(changed.map(&:id)).to eq([5,6,7,8,9])
+        end 
+
+        it 'should sorting to position back' do
+            changed = Sorting.sort models: models, changing: model, to: 1
+
+            expect(models).to eq([
+                FakeModel.new(id: 1, position: 0), 
+                FakeModel.new(id: 2, position: 2), 
+                FakeModel.new(id: 4, position: 4),
+                FakeModel.new(id: 5, position: 1),
+                FakeModel.new(id: 10, position: 9),
+                FakeModel.new(id: 7, position: 6),
+                FakeModel.new(id: 6, position: 5),
+                FakeModel.new(id: 8, position: 7),
+                FakeModel.new(id: 3, position: 3),
+                FakeModel.new(id: 9, position: 8),
+            ])
+            expect(changed.map(&:id)).to eq([5,2,3,4])
+        end 
+
+        it 'should changes nothing when changes to own position' do
+
+            changed = Sorting.sort models: models, changing: model, to: 4
+
+            expect(models).to eq([
+                FakeModel.new(id: 1, position: 0), 
+                FakeModel.new(id: 2, position: 1), 
+                FakeModel.new(id: 4, position: 3),
+                FakeModel.new(id: 5, position: 4),
+                FakeModel.new(id: 10, position: 9),
+                FakeModel.new(id: 7, position: 6),
+                FakeModel.new(id: 6, position: 5),
+                FakeModel.new(id: 8, position: 7),
+                FakeModel.new(id: 3, position: 2),
+                FakeModel.new(id: 9, position: 8),
+            ])
+            expect(changed.map(&:id)).to eq([])
+        end
+
+    end
+
+    context 'with null positions' do
+        let(:model) { FakeModel.new id: 2 }
+        let(:models) {[
+            FakeModel.new(id: 1, position: 0), 
+            FakeModel.new(id: 2, position: nil), 
+            FakeModel.new(id: 3, position: 1),
+            FakeModel.new(id: 4, position: nil),
+            FakeModel.new(id: 5, position: 2)
+        ]}
+
+        it 'should sort the model with nil position to non-nil position' do
+            changed = Sorting.sort models: models, changing: model, to: 2
+
+            expect(models).to eq([
+                FakeModel.new(id: 1, position: 0), 
+                FakeModel.new(id: 2, position: 2), 
+                FakeModel.new(id: 3, position: 1),
+                FakeModel.new(id: 4, position: 4),
+                FakeModel.new(id: 5, position: 3)
+            ])
+            expect(changed.map(&:id)).to eq([2,5])
         end
     end
 end
